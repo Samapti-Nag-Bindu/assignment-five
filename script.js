35b8f6261ef68bcb96687db87d83ef8adbc77903
Samapti Nag Bindu <jagadbandhu22@gmail.com>
2021-02-07 04:52:15 -0600

    api change

--- script.js
+++ script.js
@@ -48,7 +48,7 @@ function getMealRecipe(e) {
     e.preventDefault();
     if (e.target.classList.contains('recipe-btn')) {
         let mealItem = e.target.parentElement.parentElement;
-        fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealItem.dataset.id}`)
+        fetch(`https://www.themealdb.com/api/json/v1/1/search.php?f=${mealItem.dataset.id}`)
             .then(response => response.json())
             .then(data => mealRecipeModal(data.meals));
     }
